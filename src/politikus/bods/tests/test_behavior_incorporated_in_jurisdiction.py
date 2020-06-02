062a5352a3a359dea61d788c0d38bf912f2a2446
Khairil Yusof <khairil.yusof@gmail.com>
2020-06-02 17:34:53 +0800

    "Add behavior: incorporatedInJurisdiction"

---
+++ src/politikus/bods/tests/test_behavior_incorporated_in_jurisdiction.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from politikus.bods.behaviors.incorporated_in_jurisdiction import IIncorporatedInJurisdictionMarker
+from politikus.bods.testing import POLITIKUS_BODS_INTEGRATION_TESTING  # noqa
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from plone.behavior.interfaces import IBehavior
+from zope.component import getUtility
+
+import unittest
+
+
+class IncorporatedInJurisdictionIntegrationTest(unittest.TestCase):
+
+    layer = POLITIKUS_BODS_INTEGRATION_TESTING
+
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+
+    def test_behavior_incorporated_in_jurisdiction(self):
+        behavior = getUtility(IBehavior, 'politikus.bods.incorporated_in_jurisdiction')
+        self.assertEqual(
+            behavior.marker,
+            IIncorporatedInJurisdictionMarker,
+        )
