0f06ba70b1132c06643b20fcc7933465506106a5
Khairil Yusof <khairil.yusof@gmail.com>
2020-08-26 07:59:17 +0800

    "Add behavior: hasPepStatus"

---
+++ src/politikus/bods/tests/test_behavior_has_pep_status.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from politikus.bods.behaviors.has_pep_status import IHasPepStatusMarker
+from politikus.bods.testing import POLITIKUS_BODS_INTEGRATION_TESTING  # noqa
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from plone.behavior.interfaces import IBehavior
+from zope.component import getUtility
+
+import unittest
+
+
+class HasPepStatusIntegrationTest(unittest.TestCase):
+
+    layer = POLITIKUS_BODS_INTEGRATION_TESTING
+
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+
+    def test_behavior_has_pep_status(self):
+        behavior = getUtility(IBehavior, 'politikus.bods.has_pep_status')
+        self.assertEqual(
+            behavior.marker,
+            IHasPepStatusMarker,
+        )
