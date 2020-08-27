b68253ce8fefddff94f4c03bb2595b3f99602570
Khairil Yusof <khairil.yusof@gmail.com>
2020-08-27 10:12:20 +0800

    "Add behavior: taxResidencies"

---
+++ src/politikus/bods/tests/test_behavior_tax_residencies.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from politikus.bods.behaviors.tax_residencies import ITaxResidenciesMarker
+from politikus.bods.testing import POLITIKUS_BODS_INTEGRATION_TESTING  # noqa
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from plone.behavior.interfaces import IBehavior
+from zope.component import getUtility
+
+import unittest
+
+
+class TaxResidenciesIntegrationTest(unittest.TestCase):
+
+    layer = POLITIKUS_BODS_INTEGRATION_TESTING
+
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+
+    def test_behavior_tax_residencies(self):
+        behavior = getUtility(IBehavior, 'politikus.bods.tax_residencies')
+        self.assertEqual(
+            behavior.marker,
+            ITaxResidenciesMarker,
+        )
